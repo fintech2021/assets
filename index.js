0a92d9f0540bbc65a7a1d2e23b8318caa988abe1
82108 <shinsubin10won@gmail.com>
2021-04-28 13:32:17 +0900

    master

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-const express    = require('express');
-const mysql      = require('mysql');
-const dbconfig   = require('./config/database.js');
+const express = require("express");
+const mysql = require("mysql");
+const dbconfig = require("./config/database.js");
 const connection = mysql.createConnection(dbconfig);
 
 const app = express();
 
 // configuration =========================
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 
-app.get('/', (req, res) => {
-  res.send('Root');
+app.get("/", (req, res) => {
+  res.send("Root");
 });
 
-app.get('/users', (req, res) => {
-  connection.query('SELECT * from user', (error, rows) => {
+app.get("/users", (req, res) => {
+  connection.query("SELECT * from user", (error, rows) => {
     if (error) throw error;
-    console.log('User info is: ', rows);
+    console.log("User info is: ", rows);
     res.send(rows);
   });
 });
 
-app.listen(app.get('port'), () => {
-  console.log('Express server listening on port ' + app.get('port'));
+app.listen(app.get("port"), () => {
+  console.log("Express server listening on port " + app.get("port"));
 });
-
